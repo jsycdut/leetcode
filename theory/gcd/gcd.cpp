2c5372d7e27b1b28ed6c092e8cc3ed398298cce8
jsycdut <jsycdut@gmail.com>
2019-02-18 15:43:46 +0800

    添加辗转相除法

---
+++ theory/gcd/gcd.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+using namespace std;
+
+/**
+* 辗转相除法求二数的最大公约数
+* n数的最大公约数也可用此法
+*/
+int gcd(int a, int b)
+{
+  //应该考虑特殊情况，比如二者中
+  //有空值的情况
+  //可以一个个的判断，也可以用乘法一步到位
+  if(a * b == 0)
+  {
+    return 0;
+  }
+
+  while(a != b)
+  {
+    if( a < b )
+    {
+      b = b - a;
+    }
+    else if( a > b )
+    {
+      a = a - b;
+    }
+  }
+
+  return a;
+}
