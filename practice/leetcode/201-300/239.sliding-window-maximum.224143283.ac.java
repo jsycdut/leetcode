ee3531c5c0a376831cfebc721100b696b7e56e38
jsycdut <jsycdut@gmail.com>
2019-04-22 14:16:19 +0800

    #eetcode #39 滑动窗口中的最大值-暴力解法

---
+++ practice/leetcode/201-300/239.sliding-window-maximum.224143283.ac.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=239 lang=java
+ *
+ * [239] Sliding Window Maximum
+ *
+ * https://leetcode.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (37.61%)
+ * Total Accepted:    150.4K
+ * Total Submissions: 398.5K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * Given an array nums, there is a sliding window of size k which is moving
+ * from the very left of the array to the very right. You can only see the k
+ * numbers in the window. Each time the sliding window moves right by one
+ * position. Return the max sliding window.
+ *
+ * Example:
+ *
+ *
+ * Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+ * Output: [3,3,5,5,6,7]
+ * Explanation:
+ *
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ *
+ *
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty
+ * array.
+ *
+ * Follow up:
+ * Could you solve it in linear time?
+ */
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) return new int[0];
+
+        int[] ans = new int[nums.length - k + 1];
+
+        // 解法1，暴力解法O(M * K)，M为nums长度，K为窗口大小k
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < k; j++) {
+                max = Math.max(max, nums[i + j]);
+            }
+            ans[i] = max;
+        }
+
+        return ans;
+    }
+}
+
