9a9ea58f052c710440e9a6a63f8269d63c0747d5
jsycdut <jsycdut@gmail.com>
2019-05-08 18:13:14 +0800

    leetcode #192 bash脚本 统计文本文件的单词词频

---
+++ practice/leetcode/101-200/192.word-frequency.227577636.ac.sh
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=192 lang=bash
+#
+# [192] Word Frequency
+#
+# https://leetcode.com/problems/word-frequency/description/
+#
+# shell
+# Medium (26.86%)
+# Total Accepted:    18.4K
+# Total Submissions: 68.4K
+# Testcase Example:  'a'
+#
+# Write a bash script to calculate the frequency of each word in a text file
+# words.txt.
+#
+# For simplicity sake, you may assume:
+#
+#
+# words.txt contains only lowercase characters and space ' ' characters.
+# Each word must consist of lowercase characters only.
+# Words are separated by one or more whitespace characters.
+#
+#
+# Example:
+#
+# Assume that words.txt has the following content:
+#
+#
+# the day is sunny the the
+# the sunny is is
+#
+#
+# Your script should output the following, sorted by descending frequency:
+#
+#
+# the 4
+# is 3
+# sunny 2
+# day 1
+#
+#
+# Note:
+#
+#
+# Don't worry about handling ties, it is guaranteed that each word's frequency
+# count is unique.
+# Could you write it in one-line using Unix pipes?
+#
+#
+#
+# Read from the file words.txt and output the word frequency list to stdout.
+
+cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -rg | awk '{ print $2, $1 }'
+
