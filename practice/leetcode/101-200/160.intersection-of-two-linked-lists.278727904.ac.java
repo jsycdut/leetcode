1d34b57cfd8a50b6e6cabdefb95ecdf13fd8e21f
jsycdut <jsycdut@gmail.com>
2019-11-14 17:13:33 +0800

    feat: leetcode #160 两个链表的交点

---
+++ practice/leetcode/101-200/160.intersection-of-two-linked-lists.278727904.ac.java
@@ -0,0 +1,133 @@
+/*
+ * @lc app=leetcode id=160 lang=java
+ *
+ * [160] Intersection of Two Linked Lists
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+ *
+ * algorithms
+ * Easy (35.32%)
+ * Total Accepted:    363.5K
+ * Total Submissions: 997.4K
+ * Testcase Example:  '8\n[4,1,8,4,5]\n[5,0,1,8,4,5]\n2\n3'
+ *
+ * Write a program to find the node at which the intersection of two singly
+ * linked lists begins.
+ * 
+ * For example, the following two linked lists:
+ * 
+ * 
+ * begin to intersect at node c1.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA =
+ * 2, skipB = 3
+ * Output: Reference of the node with value = 8
+ * Input Explanation: The intersected node's value is 8 (note that this must
+ * not be 0 if the two lists intersect). From the head of A, it reads as
+ * [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5]. There are 2
+ * nodes before the intersected node in A; There are 3 nodes before the
+ * intersected node in B.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3,
+ * skipB = 1
+ * Output: Reference of the node with value = 2
+ * Input Explanation: The intersected node's value is 2 (note that this must
+ * not be 0 if the two lists intersect). From the head of A, it reads as
+ * [0,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes
+ * before the intersected node in A; There are 1 node before the intersected
+ * node in B.
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * 
+ * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB =
+ * 2
+ * Output: null
+ * Input Explanation: From the head of A, it reads as [2,6,4]. From the head of
+ * B, it reads as [1,5]. Since the two lists do not intersect, intersectVal
+ * must be 0, while skipA and skipB can be arbitrary values.
+ * Explanation: The two lists do not intersect, so return null.
+ * 
+ * 
+ * 
+ * 
+ * Notes:
+ * 
+ * 
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function
+ * returns.
+ * You may assume there are no cycles anywhere in the entire linked
+ * structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+      // 本题可以将两个链表的其中一个首尾相连成环，这样就成了典型的单链表是否有环
+      // 以及寻找环入口的问题了
+      //
+      // 边界判断
+      if (headA == null || headB == null) return null;
+
+      // 构造环
+      ListNode tailA = headA;
+      while (tailA.next != null) tailA = tailA.next;
+      tailA.next = headA;
+
+      // 快慢指针
+      ListNode runner = headB;
+      ListNode walker = headB;
+
+      while (walker != null && runner != null && runner.next != null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        // 快指针追上慢指针
+        if (runner == walker) {
+          ListNode node = headB;
+          // 寻找环入口
+          while (node != walker) {
+            node = node.next;
+            walker = walker.next;
+          }
+
+          // 解开环，还原链表结构
+          tailA.next = null;
+          return node;
+        }
+      }
+
+      // 两链表不相交，依然解开环
+      tailA.next = null;
+      return null;
+    }
+}
+
