f83957083942a716c86aa6fa4ec11fc725f1f21f
jsycdut <jsycdut@gmail.com>
2020-07-28 23:04:38 +0800

    feat: leetcode-cn #104 二叉树深度

---
+++ practice/leetcode/101-200/104.maximum-depth-of-binary-tree.java
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/submissions/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
