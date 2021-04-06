c4074ea130d930960c4f300777b0204d2c5d092c
jsycdut <jsycdut@gmail.com>
2021-04-06 13:05:54 +0800

    feat: leetcode-cn #303 区域和检索 - 数组不可变

---
+++ practice/leetcode/301-400/303.range-sum-query-immutable.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/
+ */
+class NumArray {
+    int[] sum;
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
