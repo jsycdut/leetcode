0cd6a4d5c245b799c8f373d8282b77cd7be6ca65
jsycdut <jsycdut@gmail.com>
2024-01-02 08:43:29 +0800

    feat: leetcode-cn #96 不同的二叉搜索树

---
+++ practice/leetcode/1-100/96.unique-binary-search-trees.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numTrees(int n) {
+        // 1 2 3 4 5
+        // 枚举i作为根节点的情况，[1, i]为左子树 [i+1, n]为右子树，区间左右相等则为空子树
+        // 以i作为根节点的情况下，符合条件的二叉搜索树的数量
+        // 由左子树和右子树决定，因为在根节点确定的情况下，左子树和右子树的数据是确定互不相交的
+        // 所以f(i, j) = f(1, i) * f(i+1, j)
+
+        return count(1, n);
+    }
+
+    int count(int l, int r) {
+        if (l >= r) return 1;
+        int cnt = 0;
+        for(int i = l; i <= r; i++) {
+            cnt += count(l, i-1) * count(i+1, r);
+        }
+
+        return cnt;
+    }
+}
